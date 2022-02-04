69537e01d6585a8f549e1f36febca5a7ca2527e7
Alvin(Xinyao) Sun <lucklyric@gmail.com>
2022-02-04 14:35:47 -0800

    feat(demo): A quick single page demo app

--- readme.md
+++ readme.md
@@ -1,5 +1,4 @@
-## vite-react-ts-template
-vite react ts template.
+## Amethyst Protocal SDK v2 demo page
 
 ### `yarn`
 Install dependencies.
@@ -11,21 +10,187 @@ Open http://localhost:3000 to view it in the browser.
 The page will automatically reload if you make changes to the code.
 You will see the build errors and lint warnings in the console.
 
-### `npm run lint` or `yarn lint`
-Lint codes.
+## SDK Documentation:
+### Usage: 
+Check Demo [LINK](./src/components/Content/Content.tsx)
 
-### `npm run build` or `yarn build`
-Builds the app for production to the `dist` folder.<br>
+Basic workflow🏊
+- connect to web3 provider (Metamask)
+- connect factory client to provider and signer(admin)
+- create new NFT collection by defining (name, symbol and baseURI)
+- interact with deployed NFT (ERC721)
+  - Base 721 Interfaces
+  - use admin (who created this NFT collection) to do safeMint(to, tokenId) [duplicate tokenId will be rejected by contract]
 
-The build is minified and the filenames include the hashes.<br>
+### Interfaces
+#### Factory Client
+```typescript
+export interface AmethystFactoryClient {
+    connectProvider(address: string, provider: Provider): Promise<AmethystFactoryClient>;
 
-Your app is ready to be deployed.
+    connectSigner(signer: Signer): AmethystFactoryClient;
 
-## User Guide
-React documentation: https://reactjs.org
+    setWaitConfirmations(num: number): void;
 
-Vite documentation: https://vitejs.dev/guide/features.html
+    /* ================ TRANSACTIONS ================ */
 
-React Query documentation: https://react-query.tanstack.com , https://zhuanlan.zhihu.com/p/261146977
+    /**
+     * create a new AmethystNFT collection
+     *
+     * @param {string} name
+     * @param {string} symbol
+     * @param {string} baseURI
+     * @param {PayableOverrides} [config]
+     * @return {Promise<AmethystNFTCreatedEvent>} // 交易哈希和amethystDAO地址
+     * @memberof AmethystFactoryClient
+     */
+    createAmethystNFT(
+        name: string,
+        symbol: string,
+        baseURI: string,
+        config?: PayableOverrides
+    ): Promise<AmethystNFTCreatedEvent>;
+}
+export interface TransactionEvent {
+    transactionHash: string;
+}
 
-Axios documentation: https://axios-http.com
+/* ================ AmethystFactoryClient ================ */
+
+export interface AmethystNFTCreatedEvent extends TransactionEvent {
+    amethystNFTAddress: string;
+    blockHeight: BigNumber;
+    owner: string;
+}
+```
+#### NFT Client AmethystNFT extends ERC721Client
+```typescript
+export interface AmethystNFTClient extends ERC721Client {
+    connectProvider(address: string, provider: Provider): Promise<AmethystNFTClient>;
+
+    connectSigner(signer: Signer): AmethystNFTClient;
+
+    setWaitConfirmations(num: number): void;
+
+    /**
+     * owner/admin of the contract/collection
+     *
+     * @param {CallOverrides} [config]
+     * @return {Promise<string>} owner/admin address
+     */
+    owner(config?: CallOverrides): Promise<string>;
+
+    /**
+     * safeMint
+     *
+     * @param {string} receiver address
+     * @param {BigNumber} tokenId to be minted
+     * @param {PayableOverrides} [config]
+     * @return {Promise<string>} transaction hash
+     */
+    safeMint(to: string, tokenId: BigNumber, config?: PayableOverrides): Promise<string>;
+}
+```
+```typescript
+export interface ERC721Client {
+    connectProvider(address: string, provider: Provider): Promise<ERC721Client>;
+
+    connectSigner(signer: Signer): ERC721Client;
+
+    setWaitConfirmations(num: number): void;
+
+    /* ================ VIEWS ================ */
+
+    /**
+     * 获取账户NFT余额
+     *
+     * @param {string} owner NFT所有者
+     * @param {CallOverrides} [config]
+     * @return {Promise<BigNumber>} 返回NFT个数
+     * @memberof ERC721Client
+     */
+    balanceOf(owner: string, config?: CallOverrides): Promise<BigNumber>;
+
+    /**
+     * 获取NFT所有者
+     *
+     * @param {BigNumber} tokenId NFT的tokenId
+     * @param {CallOverrides} [config]
+     * @return {Promise<string>} 返回所有者地址
+     * @memberof ERC721Client
+     */
+    ownerOf(tokenId: BigNumber, config?: CallOverrides): Promise<string>;
+
+    /**
+     * 获取NFT名称
+     *
+     * @param {CallOverrides} [config]
+     * @return {Promise<string>} 返回NFT名称
+     * @memberof ERC721Client
+     */
+    name(config?: CallOverrides): Promise<string>;
+
+    /**
+     * 获取NFT标识符
+     *
+     * @param {CallOverrides} [config]
+     * @return {Promise<string>} 返回NFT标识符
+     * @memberof ERC721Client
+     */
+    symbol(config?: CallOverrides): Promise<string>;
+
+    /**
+     * 获取NFT的metadata资源的URI地址
+     *
+     * @param {BigNumber} tokenId NFT的tokenId
+     * @param {CallOverrides} [config]
+     * @return {Promise<string>} 返回URI地址
+     * @memberof ERC721Client
+     */
+    tokenURI(tokenId: BigNumber, config?: CallOverrides): Promise<string>;
+
+    /**
+     * Total supply of the token collection
+     *
+     * @param {CallOverrides} [config]
+     * @return {Promise<BigNumber>} return NFT total supply
+     * @memberof ERC721Client
+     */
+    totalSupply(config?: CallOverrides): Promise<BigNumber>;
+
+    /**
+     * Token of owner by its index
+     *
+     * @param {string} owner address
+     * @param {BigNumber} index of token by owner
+     * @param {CallOverrides} [config]
+     * @return {Promise<BigNumber>} tokenId
+     * @memberof ERC721Client
+     */
+    tokenOfOwnerByIndex(owner: string, index: BigNumber, config?: CallOverrides): Promise<BigNumber>;
+
+    /**
+     * Token of collection by its index
+     *
+     * @param {BigNumber} index of token
+     * @param {CallOverrides} [config]
+     * @return {Promise<BigNumber>} tokenId
+     * @memberof ERC721Client
+     */
+    tokenByIndex(index: BigNumber, config?: CallOverrides): Promise<BigNumber>;
+
+    /* ================ TRANSACTIONS ================ */
+
+    /**
+     * 转移NFT
+     *
+     * @param {string} from 转出者地址
+     * @param {string} to 转入者地址
+     * @param {BigNumber} tokenId NFT的tokenId
+     * @param {PayableOverrides} [config]
+     * @return {Promise<TransactionEvent>} 返回交易hash
+     * @memberof ERC721Client
+     */
+    transferFrom(from: string, to: string, tokenId: BigNumber, config?: PayableOverrides): Promise<string>;
+}
+```
--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,22 +1,212 @@
-import React from 'react';
-import { useQuery } from 'react-query';
-import cn from 'classnames';
-
+import {
+  EtherAmethystFactoryClient,
+  EthersAmethystNFTClient
+} from '@white-matrix/amethyst-protocol-v2-sdk';
+import { BigNumber, ethers } from 'ethers';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './Content.module.less';
-import { getWeather } from '../../service/weather';
 
-interface ContentProps {
-  className?: string;
-}
+export function Content() {
+  const FACTORY_CONTRACT_ADDRESS = '0x9f1e78C90A070D35bd6f9a0587ddeF72cf1fE6B7';
 
-export function Content(props: ContentProps) {
-  const { className } = props;
+  const ethProvider = useRef<ethers.providers.Web3Provider>();
+  const factoryClient = useRef<EtherAmethystFactoryClient>();
+  const nftClient = useRef<EthersAmethystNFTClient>();
 
-  const { data, isLoading } = useQuery('getWeather', getWeather);
+  const [connectedSigner, setConnectedSigner] = useState<string>('');
+  const [factoryAddress, setFactoryAddress] = useState<string>('');
+  const [newNFTName, setNewNFTName] = useState<string>('');
+  const [newNFTSymbol, setNewNFTSymbol] = useState<string>('');
+  const [newNFTBaseURI, setNewNFTBaseURI] = useState<string>('');
+  const [lastCreatedNFTAddress, setLastCreatedNFTAddress] =
+    useState<string>('');
+
+  const [nftSymbol, setNFTSymbol] = useState<string>('');
+  const [nftOwner, setNFTOwner] = useState<string>('');
+  const [nftName, setNFTName] = useState<string>('');
+  const [nftTotalSupply, setNFTTotalSupply] = useState<string>('');
+
+  const [mintTo, setMintTo] = useState<string>('');
+  const [mintTokenId, setMintTokenId] = useState<string>('');
+  const [tokenIdToCheck, setTokenIdToCheck] = useState<string>('');
+  const [nftTokenURI, setNFTTokenURI] = useState<string>('');
+
+  const [toBalance, setToBalance] = useState<string>('');
+
+  useEffect(() => {
+    void initProvider();
+  }, []);
+
+  async function initProvider() {
+    ethProvider.current = new ethers.providers.Web3Provider(window.ethereum);
+    factoryClient.current = new EtherAmethystFactoryClient();
+    const signer = ethProvider.current.getSigner();
+    console.log(await signer.getAddress());
+    await factoryClient.current.connectProvider(
+      FACTORY_CONTRACT_ADDRESS,
+      ethProvider.current
+    );
+    factoryClient.current.connectSigner(signer);
+    factoryClient.current.setWaitConfirmations(1); // testnet
+
+    nftClient.current = new EthersAmethystNFTClient();
+  }
+
+  const refreshView = async () => {
+    const signer = await ethProvider.current?.getSigner().getAddress();
+    if (signer) {
+      setConnectedSigner(signer);
+    }
+    const factoryAddress = factoryClient.current?.amethystFactory?.address;
+    if (factoryAddress) {
+      setFactoryAddress(factoryAddress);
+    }
+  };
+
+  const refreshLastCreatedNFTView = async () => {
+    if (ethProvider.current && nftClient.current && lastCreatedNFTAddress) {
+      try {
+        await nftClient.current.connectProvider(
+          lastCreatedNFTAddress,
+          ethProvider.current
+        );
+        nftClient.current.setWaitConfirmations(1); // testnet
+        setNFTName(await nftClient.current.name());
+        setNFTSymbol(await nftClient.current.symbol());
+        setNFTOwner(await nftClient.current.owner());
+        setNFTTotalSupply((await nftClient.current.totalSupply()).toString());
+        if (mintTo !== '') {
+          setToBalance((await nftClient.current.balanceOf(mintTo)).toString());
+        }
+        if (tokenIdToCheck !== '') {
+          setNFTTokenURI(
+            await nftClient.current.tokenURI(BigNumber.from(tokenIdToCheck))
+          );
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const createNFT = async () => {
+    try {
+      if (factoryClient.current && ethProvider.current) {
+        console.log('start creating NFT');
+        const nft = await factoryClient.current.createAmethystNFT(
+          newNFTName,
+          newNFTSymbol,
+          newNFTBaseURI
+        );
+        console.log('finish');
+        console.log(nft);
+        setLastCreatedNFTAddress(nft.amethystNFTAddress);
+        await refreshLastCreatedNFTView();
+      }
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  };
+
+  const mintNFT = async () => {
+    try {
+      if (nftClient.current && ethProvider.current) {
+        nftClient.current.connectSigner(ethProvider.current.getSigner());
+        console.log('start minting NFT');
+        const tx = await nftClient.current.safeMint(
+          mintTo,
+          BigNumber.from(mintTokenId)
+        );
+        console.log('finish');
+        console.log(tx);
+        await refreshLastCreatedNFTView();
+      }
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  };
 
   return (
-    <div className={cn(styles.Content, className)}>
-      {!isLoading && data?.city}
+    <div>
+      <div className={styles.item}>
+        <button onClick={refreshView}>Refresh View</button>
+      </div>
+      <div className={styles.item}>
+        <p>
+          current connected address:{' '}
+          {connectedSigner || 'not connected or refresh to check'}
+        </p>
+      </div>
+
+      <h2>
+        Factory Interaction (nft creator will be the owner/admin of new nft
+        collection)
+      </h2>
+      <div className={styles.item}>
+        <p>current Factory address: {factoryAddress || 'refresh to check'}</p>
+      </div>
+      <div className={styles.item}>
+        NFT Name:
+        <input
+          onChange={(e) => setNewNFTName(e.target.value)}
+          placeholder="NFT Name"
+        />
+        NFT Symbol:
+        <input
+          onChange={(e) => setNewNFTSymbol(e.target.value)}
+          placeholder="NFT Symbol"
+        />
+        NFT BaseURI:
+        <input
+          onChange={(e) => setNewNFTBaseURI(e.target.value)}
+          placeholder="NFT BaseURI"
+        />
+        ,<button onClick={createNFT}>Create new AmethystNFT</button>
+      </div>
+      <h2>Last Created NFT Interaction</h2>
+      <div className={styles.item}>
+        <p>
+          Last created NFT address:{' '}
+          {lastCreatedNFTAddress || 'null or refresh to check'}
+        </p>
+      </div>
+      <div className={styles.item}>
+        <button onClick={refreshLastCreatedNFTView}>
+          Refresh Last created NFT View
+        </button>
+      </div>
+      <div className={styles.item}>
+        <p>NFT Name: {nftName || 'null or refresh to check'}</p>
+        <p>NFT Symbol: {nftSymbol || 'null or refresh to check'}</p>
+        <p>NFT Owner: {nftOwner || 'null or refresh to check'}</p>
+        <p>NFT TotalSupply: {nftTotalSupply || 'null or refresh to check'}</p>
+        <p>Current MinTo : {mintTo || 'null or refresh to check'}</p>
+      </div>
+      <h4>Mint (admin/owner) </h4>
+      <div className={styles.item}>
+        Mint to (address):
+        <input
+          onChange={(e) => setMintTo(e.target.value)}
+          placeholder="MintTo"
+        />{' '}
+        , Mint tokenId (number) duplicate will be rejected:
+        <input
+          onChange={(e) => setMintTokenId(e.target.value)}
+          placeholder="TokenId"
+        />
+        ,<button onClick={mintNFT}>mint</button>
+        <p>MinTo Current Balance : {toBalance || 'null or refresh to check'}</p>
+      </div>
+      <div className={styles.item}>
+        Token to check:
+        <input
+          onChange={(e) => setTokenIdToCheck(e.target.value)}
+          placeholder="TokenId"
+        />
+        <p>TokenURI: {nftTokenURI || 'null or refresh to check'}</p>
+      </div>
     </div>
   );
 }
